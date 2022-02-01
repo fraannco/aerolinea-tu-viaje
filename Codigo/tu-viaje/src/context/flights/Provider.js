0a35ce273dc89e6e4af920df06f9b921b7504047
francosilva25 <ernesto.silva@unmsm.edu.pe>
2022-02-01 13:38:02 -0500

    FINAL

--- Codigo/tu-viaje/src/context/flights/Provider.js
+++ Codigo/tu-viaje/src/context/flights/Provider.js
@@ -27,7 +27,7 @@ const FlightsProvider = ({ children }) => {
   const getFlights = async () => {
     try {
       const flightsResult = await apiCall({
-        url: "http://api.aviationstack.com/v1/flights?access_key=3e7263346be748c5e64b743bf9bbceb5"
+        url: "http://api.aviationstack.com/v1/flights?access_key=71dddb64516bc491741e21c0dc721270"
       });
       setFlights(flightsResult.data);
     } catch (error) {
